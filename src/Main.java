ffc236a4f6b17147025327d61b9f12fe2164cf13
Marcel Rohrbach <108031921+Mysthic1975@users.noreply.github.com>
2023-04-26 14:59:10 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,8 @@ class Main {
             }
             System.out.println("IBAN: " + iban);
             System.out.println("Prüfziffer: " + holePruefziffer(iban));
+            String bban = holeBBAN(iban);
+            System.out.println("BBAN: " + bban);
             String umformatierteIBAN = umformatiereIBAN(iban);
             StringBuilder numerischeIBAN = new StringBuilder();
             for (int i = 0; i < umformatierteIBAN.length(); i++) {
@@ -19,8 +21,15 @@ class Main {
             }
             String ibanNummer = numerischeIBAN.toString();
             System.out.println("Umformatierte IBAN: " + ibanNummer);
+            int pruefsummeModulo97 = berechnePruefsummeModulo97(ibanNummer);
+            System.out.println("Prüfsumme Modulo 97: " + pruefsummeModulo97);
             boolean ergebnis = istGueltig(iban);
             System.out.println("Ist die IBAN korrekt? " + (ergebnis ? "Ja" : "Nein"));
+
+            // Hier werden die neuen Methoden verwendet
+            System.out.println("BLZ: " + holeBLZ(iban));
+            System.out.println("Kontonummer: " + holeKontonummer(iban));
+
             System.out.print("Möchten Sie eine weitere IBAN testen? (j/n): ");
             String antwort = scanner.nextLine();
             if (!antwort.equalsIgnoreCase("j")) {
@@ -36,11 +45,16 @@ class Main {
             numerischeIBAN.append(Character.getNumericValue(umformatierteIBAN.charAt(i)));
         }
         String ibanNummer = numerischeIBAN.toString();
+        int pruefsummeModulo97 = berechnePruefsummeModulo97(ibanNummer);
+        return pruefsummeModulo97 == 1;
+    }
+
+    public static int berechnePruefsummeModulo97(String ibanNummer) {
         int mod = 0;
         for (int i = 0; i < ibanNummer.length(); i++) {
             mod = (mod * 10 + Character.getNumericValue(ibanNummer.charAt(i))) % 97;
         }
-        return mod == 1;
+        return mod;
     }
 
     public static String holePruefziffer(String iban) {
@@ -48,7 +62,20 @@ class Main {
         return String.valueOf(chars[2]) + chars[3];
     }
 
+    public static String holeBBAN(String iban) {
+        return iban.substring(4);
+    }
+
     public static String umformatiereIBAN(String iban) {
         return iban.substring(4) + iban.charAt(0) + Character.getNumericValue(iban.charAt(1)) + Character.getNumericValue(iban.charAt(2)) + Character.getNumericValue(iban.charAt(3));
+    }
+
+   
+    public static String holeBLZ(String iban) {
+        return iban.substring(4, 12);
+    }
+
+    public static String holeKontonummer(String iban) {
+        return iban.substring(12);
     }
 }
